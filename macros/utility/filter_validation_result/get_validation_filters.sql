f11c21e146442eea212d5fc01a98498f43340c21
Dat Nguyen <dat@infinitelambda.com>
2025-10-28 09:08:37 +0700

    chore: better get_validation_filters

--- macros/utility/filter_validation_result/get_validation_filters.sql
+++ macros/utility/filter_validation_result/get_validation_filters.sql
@@ -3,50 +3,53 @@
 {% endmacro %}
 
 {% macro default__get_validation_filters(validation_type) %}
-  {% if validation_type | lower == 'count' %}
-    {% set count_mismatch = namespace(
-      name='count_mismatch',
+
+  {% set all_filters = [
+    namespace(
+      name='count__mismatch',
       description='Rows where in_a != in_b',
-      macro='filter_count_validation_mismatch'
-    ) %}
-    {% set count_zero = namespace(
-      name='count_zero',
+      macro='filter_count_validation_mismatch',
+      validation_type='count'
+    ),
+    namespace(
+      name='count__equal_zero',
       description='Rows where in_a = in_b = 0',
-      macro='filter_count_validation_zero'
-    ) %}
-    {{ return([count_mismatch, count_zero]) }}
-  {% elif validation_type | lower == 'schema' %}
-    {% set schema_mismatch = namespace(
-      name='schema_mismatch',
+      macro='filter_count_validation_zero',
+      validation_type='count'
+    ),
+    namespace(
+      name='schema__mismatch_data_type',
       description='Columns where a_data_type != b_data_type',
-      macro='filter_schema_validation'
-    ) %}
-    {{ return([schema_mismatch]) }}
-  {% elif validation_type | lower == 'full' %}
-    {% set in_a_not_b = namespace(
-      name='in_a_not_b',
+      macro='filter_schema_validation',
+      validation_type='schema'
+    ),
+    namespace(
+      name='full__in_a_not_b',
       description='Rows in A but not in B',
-      macro='filter_full_validation_in_a_not_b'
-    ) %}
-    {% set in_b_not_a = namespace(
-      name='in_b_not_a',
+      macro='filter_full_validation_in_a_not_b',
+      validation_type='full'
+    ),
+    namespace(
+      name='full__in_b_not_a',
       description='Rows in B but not in A',
-      macro='filter_full_validation_in_b_not_a'
-    ) %}
-    {% set mismatch = namespace(
-      name='mismatch',
+      macro='filter_full_validation_in_b_not_a',
+      validation_type='full'
+    ),
+    namespace(
+      name='full__mismatch',
       description='Rows in both but values do not match',
-      macro='filter_full_validation_mismatch'
-    ) %}
-    {{ return([in_a_not_b, in_b_not_a, mismatch]) }}
-  {% elif validation_type | lower == 'upstream_row_count' %}
-    {% set row_count_zero = namespace(
-      name='row_count_zero',
+      macro='filter_full_validation_mismatch',
+      validation_type='full'
+    ),
+    namespace(
+      name='upstream_row_count__equal_zero',
       description='Rows where row count = 0',
-      macro='filter_upstream_row_count_validation_zero'
-    ) %}
-    {{ return([row_count_zero]) }}
-  {% else %}
-    {{ return([]) }}
-  {% endif %}
+      macro='filter_upstream_row_count_validation_zero',
+      validation_type='upstream_row_count'
+    )
+  ] %}
+
+  {% set filtered_list = all_filters | selectattr('validation_type', 'equalto', validation_type | lower) | list %}
+  {{ return(filtered_list) }}
+
 {% endmacro %}
