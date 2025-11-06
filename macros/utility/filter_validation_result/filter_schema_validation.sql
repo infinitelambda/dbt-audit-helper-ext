978df1eddb1e8269ce433e5444c7c78f1f12c025
Dat Nguyen <103571964+il-dat@users.noreply.github.com>
2025-11-06 10:57:31 +0700

    fix: default schema insight query (#23)
    * fix: correct filter_schema_validation_mismatch_data_type
    
    * fix: re-map to main

--- macros/utility/filter_validation_result/filter_schema_validation.sql
+++ macros/utility/filter_validation_result/filter_schema_validation.sql
@@ -1,5 +1,5 @@
 {% macro filter_schema_validation_mismatch_data_type(row) %}
-  {{ return(not row['HAS_DATA_TYPE_MATCH'] and row["IN_A_ONLY"]) }}
+  {{ return(not row['HAS_DATA_TYPE_MATCH'] and row["IN_BOTH"]) }}
 {% endmacro %}
 
 {% macro filter_schema_validation_errors(row) %}
