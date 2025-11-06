25e1e72101626df1ae76dae0d98305b1856ca52e
Dat Nguyen <dat@infinitelambda.com>
2025-11-06 10:54:59 +0700

    fix: re-map to main

--- macros/utility/filter_validation_result/filter_schema_validation.sql
+++ macros/utility/filter_validation_result/filter_schema_validation.sql
@@ -1,12 +1,7 @@
 {% macro filter_schema_validation_mismatch_data_type(row) %}
-  {% set has_data_type_match = audit_helper_ext.get_actual_column_name(row, 'HAS_DATA_TYPE_MATCH') %}
-  {% set in_both = audit_helper_ext.get_actual_column_name(row, 'IN_BOTH') %}
-  {{ return(not has_data_type_match and in_both) }}
+  {{ return(not row['HAS_DATA_TYPE_MATCH'] and row["IN_BOTH"]) }}
 {% endmacro %}
 
 {% macro filter_schema_validation_errors(row) %}
-  {% set has_data_type_match = audit_helper_ext.get_actual_column_name(row, 'HAS_DATA_TYPE_MATCH') %}
-  {% set in_a_only = audit_helper_ext.get_actual_column_name(row, 'IN_A_ONLY') %}
-  {% set in_b_only = audit_helper_ext.get_actual_column_name(row, 'IN_B_ONLY') %}
-  {{ return(not has_data_type_match or in_a_only or in_b_only) }}
+  {{ return(not row['HAS_DATA_TYPE_MATCH'] or row['IN_A_ONLY'] or row['IN_B_ONLY']) }}
 {% endmacro %}
